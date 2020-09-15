ac30dc2f3d2fe5ece42f1a5442637505c4185051
Sarvesh Kumar Sharma <55739302+shsarv@users.noreply.github.com>
2020-09-15 15:47:03 +0530

    Completed Day2: Basic Probability

--- Basic_probability.py
+++ Basic_probability.py
@@ -1,13 +1,12 @@
-"""Objective
+"""
+Objective
 In this challenge, we practice calculating probability. Check out the Tutorial tab for a breakdown of probability fundamentals!
 
 Task
-In a single toss of
-fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most
+In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
 
-.
 <p>2 / 3</p>
-<p>5 / 6</p>
+<p>5 / 6</p>     ------------- Correct
 <p>1 / 4</p>
 <p>1 / 6</p> 
 
